467c46fa24921a261b15e163b6cb4a2f5d3ffa4c
Nicolás Palermo <50157861+niklauspalerme@users.noreply.github.com>
2022-04-12 14:23:58 -0300

    Create usuario.js
    7) Creando el primer Modelo en Mongoose.  Modelo de Usuario

---
+++ src/models/usuario.js
@@ -0,0 +1,52 @@
+/////////////////////////////////////////////////////////////
+// Importaciones
+
+
+const { Schema, model } = require('mongoose');
+
+
+/////////////////////////////////////////////////////////////
+// Funciones
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatorio']
+    },
+    img: {
+        type: String
+    },
+    rol: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+
+})
+
+const Usuarios = model('Usuario', UsuarioSchema);
+
+
+/////////////////////////////////////////////////////////////
+// Exportamos
+
+module.exports = {
+    Usuarios
+}
