125f6b3e7aca2677263dc61f8a4764b5aa3a4fe3
Nicolás Palermo <50157861+niklauspalerme@users.noreply.github.com>
2022-04-18 13:42:47 -0300

    12) Validar rol contra base de datos usando express-validator
    12) Validar rol contra base de datos usando express-validator, se creo un modelo roles donde vamos a tener en MongoDB y lo usaremos para poder validar lo que manda el usuario en ese atributo

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,6 +6,7 @@ const { Router } = require("express");
 const { check } = require('express-validator');
 const { usuarioGet, usuarioPost, usuarioPut, usuarioDelete, usuarioPath } = require("../controllers/usuarios");
 const { validarCampos, validarEmailRepetido } = require("../middlewares/validar-campos");
+const { Roles } = require("../models/role");
 const router = Router();
 
 
@@ -19,7 +20,13 @@ router.post('/',
     check('nombre', 'The name is empty').not().isEmpty(),
     check('correo', 'The email is invalid').isEmail(),
     check('password', 'The password must be more 6 characters').isLength({ min: 6 }),
-    check('rol', 'The roll is not valid').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom(async(rol = '') => {
+        const exisRol = await Roles.findOne({ rol });
+
+        if (!exisRol) {
+            throw new Error(`The rol ${rol}, it doesnt exist on the DB`);
+        }
+    }),
     validarCampos,
     validarEmailRepetido,
     usuarioPost);
